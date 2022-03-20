72bf6438f6dd08f643bd8c9114d09d88c9159134
YavuzerenKaya <yavuzerenkaya@hotmail.com>
2022-03-20 14:29:36 +0300

    IUD

---
+++ sqlBaglanti_Inset_Update_Delete/kayit.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace sqlBaglanti_Inset_Update_Delete
+{
+    partial class kayit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openConnectionButton = new System.Windows.Forms.Button();
+            this.yaziBox2 = new System.Windows.Forms.TextBox();
+            this.veriList = new System.Windows.Forms.ListBox();
+            this.adLabel = new System.Windows.Forms.Label();
+            this.yaziBox1 = new System.Windows.Forms.TextBox();
+            this.insertButon = new System.Windows.Forms.Button();
+            this.updateButon = new System.Windows.Forms.Button();
+            this.deleteButon = new System.Windows.Forms.Button();
+            this.sadeceLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.showListButon = new System.Windows.Forms.Button();
+            this.kayitNoNumerik = new System.Windows.Forms.NumericUpDown();
+            this.kayitNumarasiLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.kayitNoNumerik)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openConnectionButton
+            // 
+            this.openConnectionButton.Location = new System.Drawing.Point(57, 261);
+            this.openConnectionButton.Name = "openConnectionButton";
+            this.openConnectionButton.Size = new System.Drawing.Size(100, 25);
+            this.openConnectionButton.TabIndex = 0;
+            this.openConnectionButton.Text = "Connection Control";
+            this.openConnectionButton.UseVisualStyleBackColor = true;
+            this.openConnectionButton.Click += new System.EventHandler(this.openConnectionButton_Click);
+            // 
+            // yaziBox2
+            // 
+            this.yaziBox2.Location = new System.Drawing.Point(90, 196);
+            this.yaziBox2.Name = "yaziBox2";
+            this.yaziBox2.Size = new System.Drawing.Size(100, 20);
+            this.yaziBox2.TabIndex = 1;
+            // 
+            // veriList
+            // 
+            this.veriList.FormattingEnabled = true;
+            this.veriList.Location = new System.Drawing.Point(325, 41);
+            this.veriList.Name = "veriList";
+            this.veriList.Size = new System.Drawing.Size(271, 381);
+            this.veriList.TabIndex = 2;
+            // 
+            // adLabel
+            // 
+            this.adLabel.AutoSize = true;
+            this.adLabel.Location = new System.Drawing.Point(2, 173);
+            this.adLabel.Name = "adLabel";
+            this.adLabel.Size = new System.Drawing.Size(82, 13);
+            this.adLabel.TabIndex = 3;
+            this.adLabel.Text = "Company Name";
+            // 
+            // yaziBox1
+            // 
+            this.yaziBox1.Location = new System.Drawing.Point(90, 170);
+            this.yaziBox1.Name = "yaziBox1";
+            this.yaziBox1.Size = new System.Drawing.Size(100, 20);
+            this.yaziBox1.TabIndex = 4;
+            // 
+            // insertButon
+            // 
+            this.insertButon.Location = new System.Drawing.Point(57, 292);
+            this.insertButon.Name = "insertButon";
+            this.insertButon.Size = new System.Drawing.Size(100, 25);
+            this.insertButon.TabIndex = 5;
+            this.insertButon.Text = "Insert";
+            this.insertButon.UseVisualStyleBackColor = true;
+            this.insertButon.Click += new System.EventHandler(this.insertButon_Click);
+            // 
+            // updateButon
+            // 
+            this.updateButon.Location = new System.Drawing.Point(57, 323);
+            this.updateButon.Name = "updateButon";
+            this.updateButon.Size = new System.Drawing.Size(100, 25);
+            this.updateButon.TabIndex = 6;
+            this.updateButon.Text = "Update";
+            this.updateButon.UseVisualStyleBackColor = true;
+            this.updateButon.Click += new System.EventHandler(this.updateButon_Click);
+            // 
+            // deleteButon
+            // 
+            this.deleteButon.Location = new System.Drawing.Point(57, 354);
+            this.deleteButon.Name = "deleteButon";
+            this.deleteButon.Size = new System.Drawing.Size(100, 25);
+            this.deleteButon.TabIndex = 7;
+            this.deleteButon.Text = "Delete";
+            this.deleteButon.UseVisualStyleBackColor = true;
+            this.deleteButon.Click += new System.EventHandler(this.deleteButon_Click);
+            // 
+            // sadeceLabel
+            // 
+            this.sadeceLabel.AutoSize = true;
+            this.sadeceLabel.Location = new System.Drawing.Point(54, 145);
+            this.sadeceLabel.Name = "sadeceLabel";
+            this.sadeceLabel.Size = new System.Drawing.Size(97, 13);
+            this.sadeceLabel.TabIndex = 9;
+            this.sadeceLabel.Text = "Kisi Ekleme Sistemi";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Location = new System.Drawing.Point(2, 203);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(41, 13);
+            this.phoneLabel.TabIndex = 13;
+            this.phoneLabel.Text = "Phone:";
+            // 
+            // showListButon
+            // 
+            this.showListButon.Location = new System.Drawing.Point(180, 325);
+            this.showListButon.Name = "showListButon";
+            this.showListButon.Size = new System.Drawing.Size(139, 54);
+            this.showListButon.TabIndex = 15;
+            this.showListButon.Text = "Show List";
+            this.showListButon.UseVisualStyleBackColor = true;
+            this.showListButon.Click += new System.EventHandler(this.showListButon_Click);
+            // 
+            // kayitNoNumerik
+            // 
+            this.kayitNoNumerik.Location = new System.Drawing.Point(90, 222);
+            this.kayitNoNumerik.Name = "kayitNoNumerik";
+            this.kayitNoNumerik.Size = new System.Drawing.Size(120, 20);
+            this.kayitNoNumerik.TabIndex = 16;
+            // 
+            // kayitNumarasiLabel
+            // 
+            this.kayitNumarasiLabel.AutoSize = true;
+            this.kayitNumarasiLabel.Location = new System.Drawing.Point(32, 229);
+            this.kayitNumarasiLabel.Name = "kayitNumarasiLabel";
+            this.kayitNumarasiLabel.Size = new System.Drawing.Size(19, 13);
+            this.kayitNumarasiLabel.TabIndex = 17;
+            this.kayitNumarasiLabel.Text = "Id:";
+            // 
+            // kayit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 450);
+            this.Controls.Add(this.kayitNumarasiLabel);
+            this.Controls.Add(this.kayitNoNumerik);
+            this.Controls.Add(this.showListButon);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.sadeceLabel);
+            this.Controls.Add(this.deleteButon);
+            this.Controls.Add(this.updateButon);
+            this.Controls.Add(this.insertButon);
+            this.Controls.Add(this.yaziBox1);
+            this.Controls.Add(this.adLabel);
+            this.Controls.Add(this.veriList);
+            this.Controls.Add(this.yaziBox2);
+            this.Controls.Add(this.openConnectionButton);
+            this.Name = "kayit";
+            this.Text = "Kayit Sistemi";
+            this.Load += new System.EventHandler(this.kayit_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kayitNoNumerik)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openConnectionButton;
+        private System.Windows.Forms.TextBox yaziBox2;
+        private System.Windows.Forms.ListBox veriList;
+        private System.Windows.Forms.Label adLabel;
+        private System.Windows.Forms.TextBox yaziBox1;
+        private System.Windows.Forms.Button insertButon;
+        private System.Windows.Forms.Button updateButon;
+        private System.Windows.Forms.Button deleteButon;
+        private System.Windows.Forms.Label sadeceLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Button showListButon;
+        private System.Windows.Forms.NumericUpDown kayitNoNumerik;
+        private System.Windows.Forms.Label kayitNumarasiLabel;
+    }
+}
+
---
+++ sqlBaglanti_Inset_Update_Delete/kayit.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace sqlBaglanti_Inset_Update_Delete
+{
+    public partial class kayit : Form
+    {
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader reader;
+        public kayit()
+        {
+            InitializeComponent();
+        }
+
+        private void kayit_Load(object sender, EventArgs e)
+        {
+            con.Close();
+        }
+
+        private void openConnectionButton_Click(object sender, EventArgs e)
+        {
+            ConnectionControl(con);
+        }
+
+        private void insertButon_Click(object sender, EventArgs e)
+        {
+            SqlInsert(cmd, reader);
+        }
+
+        private void updateButon_Click(object sender, EventArgs e)
+        {
+            UpdateThem(cmd, kayitNoNumerik);
+        }
+
+        private void deleteButon_Click(object sender, EventArgs e)
+        {
+            DeleteShipper(cmd, kayitNoNumerik);
+        }
+
+        private void showListButon_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void UpdateThem(SqlCommand command,NumericUpDown numerik )
+        {
+            try
+            {
+                SqlCon(con, "Server =.; database = Northwind; Trusted_connection = True");
+                SqlCom(command, "select * from shippers wehere ShipperID=@ID");
+                command.Parameters.AddWithValue("@ID", Math.Floor(numerik.Value));
+                ConnectionControl(con);
+                SqlReader(command, reader);
+                if (reader.HasRows)
+                {
+                    command.CommandText = "Update shippers set ('@CompanyName','@Phone')";
+                    command.Parameters.AddWithValue("@CompanyName", yaziBox1.Text);
+                    command.Parameters.AddWithValue("@Phone",yaziBox2.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Rows affected {0}", command.ExecuteNonQuery().ToString());
+                    MessageBox.Show("Istenilen oge silinmiştir.");
+                }
+                else
+                {
+                    MessageBox.Show("Verilen  numaraya ait veri bulunamdi");
+                }
+            }
+            finally
+            {
+                reader.Close();
+                SqlConnectionClose(con);
+
+            }
+        }
+        public void ConnectionControl(SqlConnection con)
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+                MessageBox.Show("baglanti kapaliydi fakat acildi");
+            }
+            else if (con.State == ConnectionState.Open)
+            {
+                MessageBox.Show("baglanti zaten acik");
+            }
+            else
+            {
+                con.Close();
+                MessageBox.Show("baglantida yasanan bir karisiklilk sebebiyle isteginiz yapilamamsitir.Lutfen tekrar deneyiniz");
+
+            }
+        }
+        public void DeleteShipper(SqlCommand command,NumericUpDown numerik)
+        {
+            try
+            {
+                SqlCon(con, "Server =.; database = Northwind; Trusted_connection = True;");
+                SqlCom(command, "select * from shippers wehere ShipperID=@ID");
+                command.Parameters.AddWithValue("@ID", Math.Floor(numerik.Value));
+                ConnectionControl(con);
+                SqlReader(command, reader);
+                if (reader.HasRows)
+                {
+                    command.CommandText = "delete from shippers where ShipperID=@ID";
+                    command.Parameters.AddWithValue("@ID", Math.Floor(numerik.Value));
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Rows affected {0}", command.ExecuteNonQuery().ToString());
+                    MessageBox.Show("Istenilen oge silinmiştir.");
+                }
+                else
+                {
+                    MessageBox.Show("Verilen  numaraya ait veri bulunamdi");
+                }
+            }
+            finally
+            {
+                reader.Close();
+                SqlConnectionClose(con);
+            }
+            
+        }
+        public void ListThem(SqlCommand command,SqlDataReader reader)
+        {
+            
+        }
+        void SqlInsert(SqlCommand command, SqlDataReader reader)
+        {
+            try
+            {
+                SqlCon(con, "Server=.; Database=Northwind; Trusted_Connection=True;");
+                SqlCom(command, "Select * From Shippers where CompanyName = @CompanyName");
+                command.Parameters.AddWithValue("@CompanyName", yaziBox1.Text);
+                ConnectionControl(con);
+                SqlDataReader rdr = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    MessageBox.Show("Bu isme ait bir şirket bulunmaktadır. Ekleme işlemi başarısız");
+
+                }
+                else
+                {
+                    SqlCom(command, "Insert into Shippers values ('@CompanyName','@Phone')");
+                    command.Parameters.AddWithValue("@CompanyName", yaziBox1.Text);
+                    command.Parameters.AddWithValue("@Phone", yaziBox2.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Rows affected" + command.ExecuteNonQuery());
+                }
+            }
+            finally
+            {
+
+                reader.Close();
+                con.Close();
+            }
+            
+        }
+
+        void SqlCom(SqlCommand command,String commandString)
+        {
+            command.CommandText = commandString;
+        }
+        void SqlCon(SqlConnection connection,string connectionString)
+        {
+            connection.ConnectionString = connectionString;
+        }
+        void SqlReader(SqlCommand command, SqlDataReader reader)
+        {
+            reader = command.ExecuteReader();
+        }
+        void SqlConnectionClose(SqlConnection connection)
+        {
+            connection.Close();
+        }
+
+    }
+}
